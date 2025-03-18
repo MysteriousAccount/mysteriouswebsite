f0009490171ac284b8eda96c966a313f0b7d1880
Nathans Lab <nathan_lab@Mac.attlocal.net>
2025-03-18 01:15:03 -0700

    ksld

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import LayerTwo from './pages/LayerTwo';
 
 const Main = () => {
   return (
-    <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+    <Routes> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/layer2' component={LayerTwo}></Route>
-    </Switch>
+    </Routes>
   );
 }
 
