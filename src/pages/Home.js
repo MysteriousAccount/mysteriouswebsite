d7fe8293c43646e820ebfb5d7757ec4229926465
Nathans Lab <nathan_lab@Mac.attlocal.net>
2025-03-18 01:12:07 -0700

    routing

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Home() {
+  return (
+    <div className="Home">
+      <div>Hello Home</div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/LayerTwo.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function LayerTwo() {
+  return (
+    <div className="LayerTwo">
+      <div>Hello layer two</div>
+    </div>
+  );
+}
+
+export default LayerTwo;
